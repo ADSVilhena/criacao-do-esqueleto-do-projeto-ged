c76c3a1ef4bcaa7b241fa20b4b88b96dfd9d405c
AllisonSBahls <allison_sousa_bahls@hotmail.com>
2019-04-03 20:42:15 -0400

    Personalização

--- GED/admin.py
+++ GED/admin.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
-
+from django.contrib.auth.models import Group
 from .models import Departamento, Funcao, Pessoa
 
-admin.site.register(Departamento)
+
+
+class DepartamentoAdmin(admin.ModelAdmin):
+    model = Departamento
+    list_display = ['nome', 'ramal']
+
+admin.site.register(Departamento, DepartamentoAdmin)
 admin.site.register(Funcao)
 admin.site.register(Pessoa)
