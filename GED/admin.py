73d5336150af4cb3961a66c5fd91c6f672023445
eclesiomelojunior <eclesiomelo.1@gmail.com>
2019-04-05 20:44:43 -0400

    Incluido Documentos, Anexos e Visualizacoes

--- GED/admin.py
+++ GED/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth.models import Group
-from .models import Departamento, Funcao, Pessoa
+from .models import Departamento, Funcao, Pessoa, Documento, Anexo, Documento_Visibilidade
 
 
 
@@ -11,3 +11,6 @@ class DepartamentoAdmin(admin.ModelAdmin):
 admin.site.register(Departamento, DepartamentoAdmin)
 admin.site.register(Funcao)
 admin.site.register(Pessoa)
+admin.site.register(Documento)
+admin.site.register(Anexo)
+admin.site.register(Documento_Visibilidade)
--- GED/models.py
+++ GED/models.py
@@ -8,6 +8,10 @@ class Departamento(models.Model):
         return self.nome
     
 class Funcao(models.Model):
+    class Meta:
+        verbose_name = 'Função'
+        verbose_name_plural = 'Funções'
+
     nome = models.CharField(max_length=100)
     def __str__(self):
         return self.nome
@@ -19,4 +23,36 @@ class Pessoa(models.Model):
     email = models.CharField(max_length=200)
     departamento = models.ForeignKey(Departamento, on_delete=models.PROTECT)
     funcao = models.ForeignKey(Funcao, on_delete=models.PROTECT)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, default=None, blank=True)
+
+    def __str__(self):
+        return self.nome
+
+class Documento(models.Model):
+    nome = models.CharField(max_length=100)
+    data_cadastro = models.DateTimeField(auto_now_add=True)
+    descricao = models.CharField(max_length=1000)
+    arquivo = models.FileField()
+    pessoa_dono = models.ForeignKey(Pessoa, related_name='pessoa_doc_dono', verbose_name="Referente a", on_delete=models.PROTECT)
+    pessoa_usuario = models.ForeignKey(User, related_name='pessoa_doc_usuario', verbose_name="Responsável", on_delete=models.PROTECT)
+    documento_privado = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.nome
+
+class Anexo(models.Model):
+    nome = models.CharField(max_length=100)
+    descricao = models.CharField(max_length=100)
+    documento = models.ForeignKey(Documento, on_delete=models.PROTECT)
+    arquivo = models.FileField()
+    def __str__(self):
+        return self.nome
+
+class Documento_Visibilidade(models.Model):
+    class Meta:
+        verbose_name = 'Visibilidade'
+        verbose_name_plural = 'Visibilidades'
+    pessoa = models.ForeignKey(Pessoa, on_delete=models.PROTECT)
+    documento = models.ForeignKey(Documento, on_delete = models.PROTECT)
+    visibilidade = models.BooleanField(default=True)
 
