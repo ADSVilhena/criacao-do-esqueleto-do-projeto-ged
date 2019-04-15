06bcd05dc35612e28da607384767002ab95cb7b4
Allison Sousa Bahls <allison_sousa_bahls@hotmail.com>
2019-04-15 16:31:04 -0400

    campo pesquisa

--- GED/urls.py
+++ GED/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import list_documentos
+from .views import list_documentos, buscar_documento
 
 urlpatterns = [
     path('', list_documentos, name = 'list_documento'),
+    path('buscar/', buscar_documento, name = 'buscar_documento'),
 ]
